56cbfca4e866474a1243e99d1b1a4e3fb54465b6
Alexandr Poslayko <aposlayko@gmail.com>
2021-06-25 09:44:52 +0300

    add templated responce

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import path from 'path';
 
 const app = express();
 const port = 5000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'templates'));
+
 app.get('/', (request, response) => {
-  response.send('Hello world!123');
+  response.render('index');
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));

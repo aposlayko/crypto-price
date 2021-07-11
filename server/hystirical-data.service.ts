533ae59f9445c626bb72a972384fd4ad2ba0441c
Alexandr Poslayko <aposlayko@gmail.com>
2021-07-11 17:24:51 +0300

    async clean up

--- server/hystirical-data.service.ts
+++ server/hystirical-data.service.ts
@@ -13,15 +13,37 @@ class HystoricalData {
   }
 
   async getData(): Promise<string> {
-    // remove file before loading if exists
-        
-    await this.getChunk('https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-01.zip');
-    await this.getChunk('https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-02.zip');
-    return await this.getChunk('https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-03.zip');    
+    const name = "BNBUSDT";
+    const interval = "1d";
+    const fileName = `${name}-${interval}.csv`;
+    const filePath = `${HystoricalData.DIR_PATH}/${fileName}`;
+
+    await this.removeFile(filePath);
+
+    await this.getChunk(
+      "https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-01.zip",
+      filePath
+    );
+    await this.getChunk(
+      "https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-02.zip",
+      filePath
+    );
+    return await this.getChunk(
+      "https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1d/BNBUSDT-1d-2019-03.zip",
+      filePath
+    );
   }
 
-  getChunk(url: string): Promise<string> {
+  async getChunk(url: string, filePath: string): Promise<string> {
     // https://data.binance.vision/data/spot/monthly/klines/BNBUSDT/1m/BNBUSDT-1m-2019-01.zip
+    const response = await this.loadData(url);
+    const unzipedContent = this.unzipFirstFile(response);
+    await this.appenToFile(unzipedContent, filePath);
+
+    return "chuks loaded";
+  }
+
+  loadData(url: string): Promise<Buffer> {
     const options: AxiosRequestConfig = {
       method: "GET",
       url,
@@ -31,34 +53,58 @@ class HystoricalData {
     return new Promise((resolve, reject) => {
       axios(options)
         .then((response) => {
-          const zip = new AdmZip(response.data);
-          const zipEntries = zip.getEntries();
-          let fileContent: string;
-
-          zipEntries.forEach(function (zipEntry) {
-            console.log(zipEntry.toString()); // outputs zip entries information
-
-            fileContent = zipEntry.getData().toString("utf8");
-          });          
-
-          fs.writeFile(
-            `${HystoricalData.DIR_PATH}/BNBUSDT.csv`,
-            fileContent,
-            { flag: "a+" },
-            (err: NodeJS.ErrnoException) => {
-              if (err) {
-                console.log(err);
-                reject(err);
-              }
-
-              resolve('chuks loaded');
-            }
-          );
+          if (response.data) {
+            resolve(response.data);
+          } else {
+            reject;
+          }
         })
         .catch((err) => {
-          console.log("Error:", err.message);
+          console.log("Error while loading data", err);
           reject(err);
         });
+    });
+  }
+
+  unzipFirstFile(zipFile: Buffer): Buffer {
+    const zip = new AdmZip(zipFile);
+    const zipEntries = zip.getEntries();
+
+    return zipEntries[0].getData();
+  }
+
+  appenToFile(content: Buffer, path: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path,
+        content,
+        { flag: "a+" },
+        (err: NodeJS.ErrnoException) => {
+          if (err) {
+            console.log("Error while writing file", err);
+            reject(err);
+          }
+
+          resolve(true);
+        }
+      );
+    });
+  }
+
+  removeFile(path: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      if (!fs.existsSync(path)) {
+        resolve(true);
+      }
+
+      fs.unlink(path, (err) => {
+        if (err) {
+          console.log("Error while remove file", err);
+          reject(false);
+        }
+
+        resolve(true);
+      });
     });
   }
 }

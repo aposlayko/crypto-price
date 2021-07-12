5baaee19fc8b70d8183e7b95a3f0640007ab80a6
Alexandr Poslayko <aposlayko@gmail.com>
2021-07-12 08:54:34 +0300

    remove old update analytics functionality

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,20 +1,8 @@
-const oldAnalyticBtn = document.getElementById("old-analytic-btn");
 const analyticBtn = document.getElementById("analytic-btn");
 
 const downloadHystoricalDataBtn = document.getElementById("download-hystorycal-data");
 const tikerInput = document.getElementById("tiker-input");
 const intervalInput = document.getElementById("interval-input");
-
-oldAnalyticBtn.addEventListener("click", () => {
-  axios
-    .post("/update-analytics/update-old")
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
 
 analyticBtn.addEventListener("click", () => {
   axios
--- server/analytic.router.ts
+++ server/analytic.router.ts
@@ -1,4 +1,4 @@
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import { CoinMarketCapService } from './coinmarketcap.service';
 import { googleService } from './google.service';
 import { TransactionsListModel } from './transactions-list.mode';
@@ -6,27 +6,12 @@ import { hystoricalData } from './hystirical-data.service';
 
 export const router = express.Router();
 
-const TAB = 'Portfolio';
-const TICKER_RANGE = 'A2:A39';
-const PRICE_RANGE = 'D2:D39';
 const SPREADSHEET_ID = '17D4eYUyrYZepfIx85B2_R6ccU9GocaVyEBsCyKoHUJ8';
 
 const TRANSACTION_TAB = 'Transactions';
 const ANALYTIC_TAB = 'Analytic';
 const TRANSACTION_RANGE = 'A2:E'
 const ANALYTIC_RANGE = 'A1:I';
-
-router.post('/update-old', async (req: any, res: any) => {
-  const response = await googleService.getCells(SPREADSHEET_ID, TAB, TICKER_RANGE);
-  const tickerList = response.filter((o) => o[0]).map((o) => o[0]);
-  const coinInfo =  await CoinMarketCapService.getCoinInfo(tickerList);
-  const responseWithPrice = response.map((o) => {
-    return o[0] ? [coinInfo[o[0]]] : o;
-  });
-  const isUpdated = await googleService.updateCells(SPREADSHEET_ID, TAB, PRICE_RANGE, responseWithPrice);
-
-  res.json({isUpdated});
-});
 
 router.post('/update', async (req, res) => {
   const response = await googleService.getCells(SPREADSHEET_ID, TRANSACTION_TAB, TRANSACTION_RANGE);
